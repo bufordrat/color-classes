600a601cf1fedfac215c95421c0a3425456c49f3
Matt Teichman <teichman@uchicago.edu>
2026-02-18 16:32:28 -0600

    not sure how to do destructuring bind yet

--- color-classes.lisp
+++ color-classes.lisp
@@ -1,1 +1,14 @@
 (defparameter x 12)
+
+(defstruct ta
+  quick-name
+  first-name
+  last-name)
+
+(defun string-to-ta (str)
+  (uiop:split-string str :separator ", "))
+
+(defparameter rat
+  (with-open-file (f "config/tas")
+    (uiop:read-file-string f)))
+
