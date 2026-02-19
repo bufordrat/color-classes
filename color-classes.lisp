0d926bca7c43a6238b8b4264d499531848006afe
Matt Teichman <teichman@uchicago.edu>
2026-02-19 13:39:28 -0600

    add students and tas

--- color-classes.lisp
+++ color-classes.lisp
@@ -1,4 +1,4 @@
-(defstruct ta
+(defstruct person
   quickname
   firstname
   lastname)
@@ -8,16 +8,37 @@
 	    (string-trim " " l))
 	  (uiop:split-string line :separator ",")))
 
-(defun string-to-ta (str)
+(defun string-to-person (str)
   (destructuring-bind (last first) 
       (process-line str)
-    (make-ta
+    (make-person
      :quickname first
      :firstname first
      :lastname last)))
 
-(defun load-tas (filepath)
+(defun load-people (filepath)
   (with-open-file (f filepath)
     (loop for line = (read-line f nil)
 	  while line
-	  collect (string-to-ta line))))
+	  collect (string-to-person line))))
+
+(defparameter tas-path
+  "config/tas")
+
+(defparameter students-path
+  "config/students")
+
+(defparameter quicknames-path
+  "config/quicknames")
+
+(defparameter tas
+  (load-people tas-path))
+
+(defparameter students
+  (load-people students-path))
+
+(defparameter colors
+  (list
+   :red
+   :orange
+   :blue))
