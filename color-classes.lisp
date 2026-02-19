5deb8af3106abcba24b8870dba483dd083712795
Matt Teichman <teichman@uchicago.edu>
2026-02-18 21:45:26 -0600

    hmm, trying a loop

--- color-classes.lisp
+++ color-classes.lisp
@@ -1,14 +1,22 @@
 (defparameter x 12)
 
 (defstruct ta
-  quick-name
-  first-name
-  last-name)
+  quickname
+  firstname
+  lastname)
 
 (defun string-to-ta (str)
-  (uiop:split-string str :separator ", "))
+  (destructuring-bind (last . first) 
+      (mapcar (lambda (s) (string-trim " " s))
+	      (uiop:split-string str :separator ","))
+    (make-ta
+     :quickname first
+     :firstname first
+     :lastname last)))
 
 (defparameter rat
   (with-open-file (f "config/tas")
+    (loop for line = (read-line f nil :eof)
+	  until (eq line :eof)
+	  collect (string-to-ta line))
     (uiop:read-file-string f)))
-
